655a1a41773d2284f93f1a773056bf19d830179f
shouwang <455542328@qq.com>
2019-01-05 14:38:32 +0800

    第一天

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// 入口文件
+import Vue from 'vue'
+
+//按需导入Mint-ui组件
+import { Header } from 'mint-ui';
+
+//导入mui样式
+import './lib/mui/css/mui.min.css'
+
+Vue.component(Header.name, Header);
+
+//导入App组件
+import app from './App.vue'
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app)
+})
